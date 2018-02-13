4604ca9ad0cca199374d75a6344ec37e9b0f9ce1
racf <racf1991@gmail.com>
2018-02-13 02:37:55 -0600

    Añadio Drawer

--- app/src/main/java/com/untitledev/monitoreofamiliar/activities/HomeActivity.java
+++ app/src/main/java/com/untitledev/monitoreofamiliar/activities/HomeActivity.java
@@ -1,15 +1,52 @@
 package com.untitledev.monitoreofamiliar.activities;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.untitledev.monitoreofamiliar.R;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //Mandar a llamar el toolbar una vez generado en el activity_main de la actividad
+        setToolbar();
+
+        setToolbar();
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navView);
+        //setFragmentDefault();
+    }
+
+    /*private void setFragmentDefault(){
+        changeFragment(new EmailFragment(), navigationView.getMenu().getItem(0));
+    }*/
+
+    private void setToolbar(){
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home_dark);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                //Abrir el menu lateral
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
